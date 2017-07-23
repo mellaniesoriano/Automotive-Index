18570079d3c9eb92a43496fb9870bbca5fce5ff5
mellaniesoriano <msoriano808@gmail.com>
2017-07-22 18:44:12 -1000

    added new times with indexes

--- indexing.sql
+++ indexing.sql
@@ -15,18 +15,24 @@ CREATE INDEX title_index ON car_models(make_title) WHERE make_code LIKE '%LAM%';
 SELECT DISTINCT make_title FROM car_models WHERE make_code LIKE '%LAM%';
 -- Time : 29.501 ms
 
-CREATE INDEX title_index ON car_models(make_title) WHERE make_code LIKE '%NISSAN%' AND model_code LIKE '%GT-R%';
--- Time : 0.473 ms
+CREATE INDEX gtr ON car_models(make_title) WHERE make_code LIKE '%NISSAN%' AND model_code LIKE '%GT-R%';
+-- Time: 2.307 ms
 SELECT DISTINCT model_title FROM car_models WHERE make_code LIKE '%NISSAN%' AND model_code LIKE '%GT-R%';
 -- Time : 32.251 ms
 
-
+CREATE INDEX all_lam_index ON car_models(make_title) WHERE make_code LIKE '%LAM%';
+-- Time : Time: 2.459 ms
 SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code LIKE '%LAM%';
 -- Time : 30.273 ms
 
+
+CREATE INDEX years_between_2010_and_2015_index ON car_models(make_title) WHERE year BETWEEN 2010 AND 2015;
+-- Time : Time: 56.487 ms
 SELECT * FROM car_models WHERE year BETWEEN 2010 AND 2015;
 -- Time : 83.180 ms
 
+CREATE INDEX year_is_2010 ON car_models(make_title) WHERE year = 2010;
+-- Time : 13.644 ms
 SELECT * FROM car_models WHERE year = 2010;
 -- Time : 35.467 ms
 
