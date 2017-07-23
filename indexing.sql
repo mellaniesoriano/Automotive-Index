ec69f7888e556ab3fcb75311664b837bda711260
mellaniesoriano <msoriano808@gmail.com>
2017-07-22 16:11:45 -1000

    3 & 4 done

--- indexing.sql
+++ indexing.sql
@@ -8,6 +8,14 @@ CREATE DATABASE indexed_cars WITH OWNER indexed_cars_user;
 
 \i scripts/car_model_data.sql;
 
+-- SELECT COUNT(id) [ COLUMN NAME ] to check count
+
 SELECT DISTINCT make_title FROM car_models WHERE make_code LIKE '%LAM%';
 
 SELECT DISTINCT model_title FROM car_models WHERE model_code LIKE '%GT-R%';
+
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code LIKE '%LAM%';
+-- Time: 30.273 ms
+
+SELECT * FROM car_models WHERE year BETWEEN 2010 AND 2015;
+-- Time: 83.180 ms
