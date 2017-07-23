c16f741428eed8d32c5b5ce63b12c30ea20b3d36
mellaniesoriano <msoriano808@gmail.com>
2017-07-22 16:40:37 -1000

    compared timing, done

--- indexing.sql
+++ indexing.sql
@@ -6,23 +6,27 @@ CREATE DATABASE indexed_cars WITH OWNER indexed_cars_user;
 
 \c indexed_cars;
 
-\i scripts/cars_models.sql;
+\i scripts/cars_models.sql
 
-\i scripts/car_model_data.sql;
-
--- SELECT COUNT(id) [ COLUMN NAME ] to check count
+\i scripts/car_model_data.sql
 
 SELECT DISTINCT make_title FROM car_models WHERE make_code LIKE '%LAM%';
--- Time: 29.501 ms
+-- 1st Time : 29.501 ms
+-- 2nd Time : 25.949 ms
 
 SELECT DISTINCT model_title FROM car_models WHERE model_code LIKE '%GT-R%';
--- Time: 33.513 ms
+-- 1st Time : 33.513 ms
+-- 2nd Time : 32.511 ms
 
 SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code LIKE '%LAM%';
--- Time: 30.273 ms
+-- 1st Time : 30.273 ms
+-- 2nd Time : 26.248 ms
 
 SELECT * FROM car_models WHERE year BETWEEN 2010 AND 2015;
--- Time: 83.180 ms
+-- 1st Time : 83.180 ms
+-- 2nd Time : 71.748 ms
 
 SELECT * FROM car_models WHERE year = 2010;
--- Time: 35.467 ms
+-- 1st Time : 35.467 ms
+-- 2nd Time : 29.852 ms
+
